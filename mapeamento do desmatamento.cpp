5a285b3b2a173e3136bcf91abd5d2b9cada38d30
gabb978 <cnainformatica2015@gmail.com>
2024-05-21 21:10:31 -0300

    mapeamento do desmatamento

---
+++ mapeamento do desmatamento.cpp
@@ -0,0 +1,98 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+int interesse,problemas,questoes;
+char r='s',continuar;
+while(r=='s'){
+
+	printf("                         BEM-VINDO(A) AO CENTRO DE INFORMACOES DE MAPEAMENTO DO DESMATAMENTO\n");
+	printf("\n");
+	printf("                                          ESCOLHA UMA DAS AREAS AFETADAS\n");
+	printf("\n");
+	printf("                                                  1- AMAZONIA \n");
+	printf("                                                  2- PARA     \n");
+	scanf("%d",&interesse);
+		switch(interesse){
+			case 1:
+ 				printf("\n");
+				printf("                                                 AMAZONIA\n");
+				printf("                                 A AMAZONIA E A MAIOR FLORESTA TROPICAL DO MUNDO,\n");
+				printf("                                 LOCALIZADA NA REGIAO DA BACIA AMAZONICA NA\n");
+				printf("                                 AMERICA DO SUL.\n");
+				printf("\n");
+				printf("                               QUAIS DESSSAS QUESTOES LHE DESPERTA MAOIR INTERESSE\n");
+				printf("                                        1- INFORMACOES DO DESMATAMENTO\n");
+				printf("                                        2- INFORMACOES DO REFLORESTAMENTO\n");
+				scanf("%d",&questoes);
+			switch(questoes){
+				case 1:
+					printf(" 1- AREAS DESMATADAS\n");
+					printf(" 2- CAUSAS\n");
+					printf(" 3- TRIBOS AFETADAS\n");
+					printf(" 4- ESPECIES AFETADAS\n");
+					printf(" 5- ARVORES EXTINTAS\n");
+					scanf("%d",&problemas);
+				switch(problemas){
+					case 1:
+						printf("                                    AREAS DESMATADAS\n");
+						printf(" \n");
+						printf("                    EM 2022, FORAM DEVASTADOS 10.573Km² EM TERRITORIO \n");
+						printf("                    AMAZONICO BRASILEIRO, A MAIOR DESTRUICAO DESDE 2008.\n");
+						printf("                    NO ENTANTO, E IMPORTANTE RESSALTAR QUE O DESMATAMENTO\n");
+						printf("                    NAO ESTA RESTRITO A BAIRROS ESPECIFICOS, MAIS SIM A AREAS\n");
+						printf("                    MAIS AMPLAS DA FLORESTA\n");
+						printf("\n");
+						printf(" QUE CONTINUAR O PROGRAMA?\n");
+						scanf("%s",&r);
+						break;
+					case 2:
+						printf("                             PRINCIPAIS CAUSAS DO DESMATAMENTO\n"),
+						printf("\n");
+						printf("                      PRIMEIRO MOTIVO E A IMPUNIDADE DE CRIMES AMBIENTAIS,\n");
+						printf("                      ATIVIDADE PECUARIA, PROJETOS DE EXTRACAO DE MADEIRA,\n");
+						printf("                      MINERACAO, ESTIMULO A GRILAGEM DE TERRAS PUBLICAS,\n");
+						printf("                      RETOMADA DE GRANDES OBRAS.");
+						printf("\n");
+						printf(" QUE CONTINUAR O PROGRAMA?\n");
+						scanf("%s",&r);
+						break;
+					case 3:
+						printf("                             TRIBOS AFETADAS PELO DESMATAMENTO\n");
+						printf("\n");
+						printf("                       CERCA DE 23 POR CENTO DO TERRITORIO AMAZONICO E CONSTITUIDO\n");
+						printf("                       POR TERRAS INDIGENAS, AS TERRAS INDIGENAS MAIS AFETADAS PELO\n");
+						printf("                       DESMATAMENTO INCLUEM O TERRITORIO YANOMAMI, KAYAPO, MUNDURUKU\n");
+						printf("\n");
+						printf(" QUE CONTINUAR O PROGRAMA?\n");
+						scanf("%s",&r);
+						break;
+					case 4:
+						printf("                           ESPECIES AFETADAS PELO DESMATAMENTO\n");
+						printf("\n");
+						printf("                       AS PRINCIPAIS ESPECIES AFETADAS SAO A ONCA-PINTADA,\n");
+						printf("                       TAMANDUA-BANDEIRA, PEIXE-BOI-DA-AMAZONIA, ARARAJUBA\n");
+						printf("                       E ANTA");
+						printf("\n");
+						printf(" QUE CONTINUAR O PROGRAMA?\n");
+						scanf("%s",&r);
+						break;
+					case 5:
+						printf("                                     ARVORES EXTINTAS\n");
+						printf("\n");
+						printf("                              MUITAS QUE ESTAO AMEACADAS DE EXTINCAO SAO:\n");
+						printf("                              1. ANDIROBA (Carapa Guianensis Aubl)\n");
+						printf("                              2. MOGNO BRASILEIRO (Swietenia Macrophylla)\n");
+						printf("                              3. PAU-ROSA (Aniba Rosaedora Ducke)\n");
+						printf("                              4. CASTANHEIRA(Berthulletia Excelsa)\n");
+						break;
+						
+
+}
+}
+break;
+case 2:					
+printf("aaaaaa\n");							
+}
+}
+}
