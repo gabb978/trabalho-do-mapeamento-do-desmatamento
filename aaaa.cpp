bd0d47d63a60b90a3cffe8a5d3ec128b32ea7d17
gabb978 <cnainformatica2015@gmail.com>
2024-06-04 15:14:31 -0300

    refloretamento prá

---
+++ aaaa.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+int main()
+{
+	SetConsoleOutputCP(CP_UTF8);
+int interesse,problemas,questoes,questao1, questao2,questao3,questao4;
+char jogo,r='s',continuar,r2='s',prosseguir,prosseguir2,prosseguir4,r3='s';
+while(r=='s'){
+
+						printf(" 1-COMO FUNCIONA O RESFLORESTAMENTO\n");
+						printf("\n");
+						printf("2-FAÇA O REFLORESTAMENTO NA PRATICA\n ");
+						printf("\n");
+	                    scanf("%d",&problemas);
+				        switch(problemas){
+				        	
+				        	
+				        	case 1:	
+							printf("\n");
+							printf("                               COMO FUNCIONA O REFLORESTAMENTO\n");
+					printf("\n");
+					printf("                        O REFLORESTAMENTO NADA MAIS É QUE UMA TÉCNCA QUE\n");
+					printf("                        RECUPERA ÁREAS QUE FORAM DESMATADAS OU DEGRADADAS,\n");
+					printf("                        EXISTE DOIS TIPOS DE REFLORESTAMENTO O URBANO E\n");
+					printf("                        RURAL, AS TÉCNICAS USADAS PARA O REFLORESTAMENTO\n");
+					printf("                        É NADA MAIS NADA MENOS QUE O PLANTIO DE MUDAS\n");
+					printf("\n");
+					printf(" QUER PARTICIPAR DO JOGO DO RFLORESTAMENTO [s/n]\n");
+					scanf(" %s",&jogo);
+					if (jogo=='s'){
+						while(r2=='s'){
+}
+}
+}
+}
+}
